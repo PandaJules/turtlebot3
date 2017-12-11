a2f163ac527f672a5b8a7c80fb064e4fc77a06cb
Julia <yb259@cam.ac.uk>
2017-12-11 05:28:42 +0000

    use teleop and take shots with SPACE

--- scripts/analysis/noise.py
+++ scripts/analysis/noise.py
@@ -4,8 +4,23 @@ import rospy
 from geometry_msgs.msg import Twist
 from sensor_msgs.msg import LaserScan
 import numpy as np
+import random
+import sys, select, termios, tty
+import matplotlib.pyplot as plt
 scan = []
 max_range = 50
+
+
+def get_key():
+    tty.setraw(sys.stdin.fileno())
+    rlist, _, _ = select.select([sys.stdin], [], [], 0.1)
+    if rlist:
+        k = sys.stdin.read(1)
+    else:
+        k = ''
+
+    termios.tcsetattr(sys.stdin, termios.TCSADRAIN, settings)
+    return k
 
 
 def update_laser_scan(scan_msg):
@@ -21,8 +36,10 @@ def filter_front_scan(laser_scan):
 
 def take_one_front_shot():
     front = filter_front_scan(scan)
+    # front = [random.uniform(0, 4) for _ in range(181)]
     angles = range(-90, 91)
-    plot(angles, front)
+    plt.plot(angles, front)
+    plt.show()
 
 
 if __name__ == "__main__":
@@ -33,9 +50,54 @@ if __name__ == "__main__":
 
     r = rospy.Rate(5)
 
-    while not rospy.is_shutdown():
-        if key_pressed == Space:
-            take_one_front_shot()
-        else:
-            move_around()
+    settings = termios.tcgetattr(sys.stdin)
 
+    target_linear_vel = 0
+    target_angular_vel = 0
+    control_linear_vel = 0
+    control_angular_vel = 0
+    try:
+        while not rospy.is_shutdown():
+            key = get_key()
+            if key == 'w':
+                target_linear_vel = target_linear_vel + 0.01
+            elif key == 'x':
+                target_linear_vel = target_linear_vel - 0.01
+            elif key == 'a':
+                target_angular_vel = target_angular_vel + 0.1
+            elif key == 'd':
+                target_angular_vel = target_angular_vel - 0.1
+            elif key == 's':
+                target_linear_vel = 0
+                control_linear_vel = 0
+                target_angular_vel = 0
+                control_angular_vel = 0
+            elif key == ' ':
+                take_one_front_shot()
+            else:
+                if key == '\x03':
+                    break
+
+            if target_linear_vel > control_linear_vel:
+                control_linear_vel = min(target_linear_vel, control_linear_vel + (0.01 / 4.0))
+            else:
+                control_linear_vel = target_linear_vel
+
+            if target_angular_vel > control_angular_vel:
+                control_angular_vel = min(target_angular_vel, control_angular_vel + (0.1 / 4.0))
+            else:
+                control_angular_vel = target_angular_vel
+
+            twist = Twist()
+            twist.linear.x = control_linear_vel
+            twist.angular.z = control_angular_vel
+            pub.publish(twist)
+
+    except Exception as e:
+        print(e)
+
+    finally:
+        twist = Twist()
+        pub.publish(twist)
+
+    termios.tcsetattr(sys.stdin, termios.TCSADRAIN, settings)
