56264d06c30cb82091a366bd145d59c8a7a32404
PandaJules <bibik.julia.96@gmail.com>
2018-01-09 23:48:43 +0000

    Rethinking logic in k-maps to avoid crashing on the sides when going forward

--- scripts/controllers/rule_based_5sensors.py
+++ scripts/controllers/rule_based_5sensors.py
@@ -4,6 +4,7 @@ import numpy as np
 import rospy
 from geometry_msgs.msg import Twist
 from nav_msgs.msg import Odometry
+from math import degrees, radians
 from sensor_msgs.msg import LaserScan, JointState
 from tf.transformations import euler_from_quaternion
 import os
@@ -13,19 +14,20 @@ import os
 ***************************"""
 
 PI = 3.14159265
-half_wheel_separation = 0.08
-rotate_angle = 45.0 * PI / 180
-front_distance_limit = 0.7
-side_distance_limit = 0.4
-wheel_radius = 0.033
+half_wheel_separation = 0.07
+rotate_angle = radians(45)
+front_distance_limit = 0.8
+oblique_distance_limit = 0.6
+side_distance_limit = 0.5
+wheel_radius = 0.03
 right_joint_encoder = 0.0
-priv_right_joint_encoder = 0.0
+new_right_joint_encoder = 0.0
 direction_vector = [0]*5
 # States for FSM
 GET_DIRECTION = 0
 DRIVE_FORWARD = 1
-RIGHT_TURN = 2
-LEFT_TURN = 3
+LEFT_TURN = 2
+RIGHT_TURN = 3
 # 5 sensors to the WEST, NW, N, NE, EAST
 CENTER = 0
 NW = 1
@@ -34,6 +36,9 @@ RIGHT = 3
 NE = 4
 # State can be one of get_direction, driving forward, turning left or right
 turtlebot3_state = 0
+turtlebot3_lin_vel = 0
+turtlebot3_ang_vel = 0
+theta = 0
 
 
 def jointStateMsgCallBack(joint_state_msg):
@@ -42,7 +47,7 @@ def jointStateMsgCallBack(joint_state_msg):
 
 
 def laserScanMsgCallBack(laser_msg):
-    global direction_vector
+    global direction_vector, dv
     angles = [0, 45, 90, -90, -45]
 
     for counter, angle in enumerate(angles):
@@ -50,6 +55,11 @@ def laserScanMsgCallBack(laser_msg):
             direction_vector[counter] = laser_msg.range_max
         else:
             direction_vector[counter] = laser_msg.ranges[angle]
+    # dv[0] = direction_vector[2] < front_distance_limit
+    # dv[1] = direction_vector[1] < oblique_distance_limit
+    # dv[2] = direction_vector[0] < side_distance_limit
+    # dv[3] = direction_vector[4] < oblique_distance_limit
+    # dv[4] = direction_vector[3] < side_distance_limit
 
 
 def updateCommandVelocity(linear, angular):
@@ -59,55 +69,99 @@ def updateCommandVelocity(linear, angular):
     cmd_vel_pub.publish(cmd_vel)
 
 
+def update_angle(odom_msg):
+    global theta
+    quaternion = odom_msg.pose.pose.orientation
+    (roll, pitch, theta) = euler_from_quaternion([quaternion.x,
+                                                  quaternion.y,
+                                                  quaternion.z,
+                                                  quaternion.w])
+
+
+def cmdMsgCallBack(cmd_msg):
+    global turtlebot3_lin_vel, turtlebot3_ang_vel
+    turtlebot3_lin_vel = cmd_msg.linear.x
+    turtlebot3_ang_vel = cmd_msg.angular.z
+
+
 """"*******************************************************************************
 * Control Loop function
 *******************************************************************************"""
 
 
 def controlLoop():
-    global priv_right_joint_encoder, turtlebot3_state
-    wheel_rotation_angle = (rotate_angle * half_wheel_separation / wheel_radius)
-    linear_vel_x = 0.3
-    angular_vel_z = 1.5
+    global new_right_joint_encoder, turtlebot3_state
+    wheel_rotation_angle = 1.01*(rotate_angle * half_wheel_separation / wheel_radius)
+    cmd_vel = Twist()
+    cmd_vel.linear.x = 0.25
+    rot_vel_left = Twist()
+    rot_vel_left.angular.z = 1.57
+    rot_vel_right = Twist()
+    rot_vel_right.angular.z = -1.57
+    t=0
 
     if turtlebot3_state == GET_DIRECTION:
         """Normally TURN RIGHT, unless there is a wall, then either TURN LEFT or GO FORWARD"""
+        # if direction_vector[LEFT] < side_distance_limit:
+        #     if direction_vector[NW] > side_distance_limit > direction_vector[NE]:
+        #         new_right_joint_encoder = right_joint_encoder + wheel_rotation_angle
+        #         turtlebot3_state = LEFT_TURN
+        #     elif direction_vector[CENTER] < front_distance_limit:
+        #         new_right_joint_encoder = right_joint_encoder - wheel_rotation_angle
+        #         turtlebot3_state = RIGHT_TURN
+        #     else:
+        #         turtlebot3_state = DRIVE_FORWARD
+        #
+        # else:
+        #     if direction_vector[NE] > side_distance_limit > direction_vector[NW] \
+        #             and direction_vector[CENTER] < front_distance_limit:
+        #         new_right_joint_encoder = right_joint_encoder - wheel_rotation_angle
+        #         turtlebot3_state = RIGHT_TURN
+        #     elif direction_vector[CENTER] > front_distance_limit:
+        #         turtlebot3_state = DRIVE_FORWARD
+        #     else:
+        #         new_right_joint_encoder = right_joint_encoder + wheel_rotation_angle
+        #         turtlebot3_state = LEFT_TURN
+
         if direction_vector[LEFT] < side_distance_limit:
-            if direction_vector[NW] > side_distance_limit > direction_vector[NE]:
-                priv_right_joint_encoder = right_joint_encoder + wheel_rotation_angle
+            if direction_vector[NW] > oblique_distance_limit > direction_vector[NE]:
                 turtlebot3_state = LEFT_TURN
-            elif direction_vector[CENTER] < front_distance_limit:
-                priv_right_joint_encoder = right_joint_encoder - wheel_rotation_angle
+            elif direction_vector[CENTER] < front_distance_limit or direction_vector[NW] < oblique_distance_limit:
+                t = degrees(theta % (2 * PI))
                 turtlebot3_state = RIGHT_TURN
             else:
                 turtlebot3_state = DRIVE_FORWARD
 
         else:
-            if direction_vector[NE] > side_distance_limit > direction_vector[NW] \
-                    and direction_vector[CENTER] < front_distance_limit:
-                priv_right_joint_encoder = right_joint_encoder - wheel_rotation_angle
-                turtlebot3_state = RIGHT_TURN
-            elif direction_vector[CENTER] > front_distance_limit:
+            if direction_vector[NE] < oblique_distance_limit and \
+                    (direction_vector[NW] > oblique_distance_limit or direction_vector[CENTER] < front_distance_limit):
+                turtlebot3_state = LEFT_TURN
+            elif direction_vector[CENTER] > front_distance_limit and direction_vector[NW] > oblique_distance_limit:
                 turtlebot3_state = DRIVE_FORWARD
             else:
-                priv_right_joint_encoder = right_joint_encoder + wheel_rotation_angle
-                turtlebot3_state = LEFT_TURN
+                t = degrees(theta % (2 * PI))
+                turtlebot3_state = RIGHT_TURN
 
     elif turtlebot3_state == DRIVE_FORWARD:
-        updateCommandVelocity(linear_vel_x, 0.0)
+        cmd_vel_pub.publish(cmd_vel)
+        turtlebot3_state = GET_DIRECTION
+
+    elif turtlebot3_state == LEFT_TURN:
+        new_right_joint_encoder = right_joint_encoder + wheel_rotation_angle
+        while abs(new_right_joint_encoder - right_joint_encoder) > 0.05:
+            cmd_vel_pub.publish(rot_vel_left)
+
         turtlebot3_state = GET_DIRECTION
 
     elif turtlebot3_state == RIGHT_TURN:
-        if abs(priv_right_joint_encoder - right_joint_encoder) < 0.1:
-            turtlebot3_state = GET_DIRECTION
-        else:
-            updateCommandVelocity(0.0, -1 * angular_vel_z)
-
-    elif turtlebot3_state == LEFT_TURN:
-        if abs(priv_right_joint_encoder - right_joint_encoder) < 0.1:
-                turtlebot3_state = GET_DIRECTION
-        else:
-            updateCommandVelocity(0.0, angular_vel_z)
+        new_right_joint_encoder = right_joint_encoder - wheel_rotation_angle
+        t = degrees(theta % (2 * PI))
+        a = right_joint_encoder
+        while abs(new_right_joint_encoder - right_joint_encoder) > 0.05:
+            cmd_vel_pub.publish(rot_vel_right)
+        print(a - right_joint_encoder)
+        print(abs(degrees(theta % (2 * PI)) - t))
+        turtlebot3_state = GET_DIRECTION
 
     else:
         turtlebot3_state = GET_DIRECTION
@@ -124,10 +178,12 @@ if __name__ == "__main__":
     rospy.loginfo("robot_model : BURGER")
     rospy.loginfo("To stop TurtleBot CTRL + C")
 
-    cmd_vel_pub = rospy.Publisher('/cmd_vel', Twist, queue_size=5)
+    cmd_vel_pub = rospy.Publisher('/cmd_vel', Twist, queue_size=2)
+    cmd_vel_sub = rospy.Subscriber('/cmd_vel', Twist, cmdMsgCallBack)
 
     laser_scan_sub = rospy.Subscriber('/scan', LaserScan, laserScanMsgCallBack)
     joint_state_sub = rospy.Subscriber('/joint_states', JointState, jointStateMsgCallBack)
+    odometry_sub = rospy.Subscriber('/odom', Odometry, update_angle)
 
     r = rospy.Rate(125)
     while not rospy.is_shutdown():
