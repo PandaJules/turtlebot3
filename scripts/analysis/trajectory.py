a6b978f2036c69d517347ff0d21a584fee887934
PandaJules <bibik.julia.96@gmail.com>
2018-03-07 23:12:05 +0000

    Keeping track of the number of full laps

--- scripts/analysis/trajectory.py
+++ scripts/analysis/trajectory.py
@@ -11,6 +11,7 @@ from gazebo_msgs.msg import ModelState
 (x_sim, y_sim) = 0, 0
 (x_sim_prev, y_sim_prev) = 0, 0
 LOG_PATH = os.path.join(os.path.expanduser('~'), "Desktop")
+at_cross_line = False
 
 
 def odomMsgCallBack(odom_msg):
@@ -20,25 +21,34 @@ def odomMsgCallBack(odom_msg):
 
 
 def track():
-    global x_sim_prev, y_sim_prev, number_of_laps
+    global x_sim_prev, y_sim_prev, number_of_laps, at_cross_line
+    i = int(input("Which log file to write to: -1 for automatic detection / any other integer for your choice\n"))
     filename = LOG_PATH + "/logs/trajectory_log_"
-    i = 1
-    while os.path.exists('{}{:d}.txt'.format(filename, i)):
-        i += 1
+    if i == -1:
+        i = 1
+        while os.path.exists('{}{:d}.txt'.format(filename, i)):
+            i += 1
     filename = '{}{:d}.txt'.format(filename, i)
 
     print("Writing a log file of trajectories to {}".format(filename))
     try:
         with open(filename, 'a') as tlog:
             while 1:
-                if round(x_sim_prev, 5) != round(x_sim, 5) or round(y_sim_prev, 5) != round(y_sim, 5):
+                if round(x_sim_prev, 4) != round(x_sim, 4) or round(y_sim_prev, 4) != round(y_sim, 4):
                     tlog.write('{:.6f},{:.6f}\n'.format(x_sim, y_sim))
                     x_sim_prev = x_sim
                     y_sim_prev = y_sim
+                    # print('{:5f},{:5f}'.format(round(x_sim, 3), round(y_sim, 3)))
+
+                    if abs(round(x_sim, 3) - start_x) < 0.01 and start_y-1 < round(y_sim, 3) < start_y+1:
+                        if not at_cross_line:
+                            at_cross_line = True
+                            number_of_laps += 1
+                            print("New lap. Number {}".format(number_of_laps))
+                            r.sleep()
+                    else:
+                        at_cross_line = False
                 r.sleep()
-                if round(x_sim,1) == start_x and start_y-2<round(y_sim,1)<start_x-2:
-                    number_of_laps += 1
-                    print("New lap {}".format(number_of_laps))
     except Exception as e:
         print(e)
 
@@ -51,10 +61,11 @@ if __name__ == "__main__":
     reset_world = rospy.ServiceProxy("/gazebo/reset_world", Empty)
 
     r = rospy.Rate(100)
-    r.sleep(10)
+    r.sleep()
     if (x_sim, y_sim) != (0, 0):
-        start_x = x_sim
-        start_y = y_sim
+        start_x = round(x_sim, 2)
+        start_y = round(y_sim, 2)
+        print("Start coordinates are {:.3f},{:.3f}".format(start_x, start_y))
     # model_state_msg = ModelState()
     # model_state_msg.model_name = 'turtlebot3_burger'
     # model_state_msg.pose.orientation.x = 0
@@ -66,5 +77,5 @@ if __name__ == "__main__":
     # model_state_msg.pose.position.z = 0
     # model_state_msg.twist.linear.x = 0
     # model_state_msg.twist.angular.z = 0
-    number_of_laps = 0
+    number_of_laps = 1
     track()
