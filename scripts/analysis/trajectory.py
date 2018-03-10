26126f3b810fc081dbd933aa249756b8f10c3884
PandaJules <bibik.julia.96@gmail.com>
2018-03-10 09:56:19 +0000

    Functionality for searching for hyperparameter - weights in a grid

--- scripts/analysis/trajectory.py
+++ scripts/analysis/trajectory.py
@@ -2,16 +2,16 @@
 
 import os
 import rospy
-from geometry_msgs.msg import Twist
 from nav_msgs.msg import Odometry
-from std_srvs.srv import Empty
-from gazebo_msgs.msg import ModelState
+from std_msgs.msg import Int32, Bool
 
 
 (x_sim, y_sim) = 0, 0
 (x_sim_prev, y_sim_prev) = 0, 0
 LOG_PATH = os.path.join(os.path.expanduser('~'), "Desktop")
 at_cross_line = False
+
+search_exhausted = False
 
 
 def odomMsgCallBack(odom_msg):
@@ -20,35 +20,89 @@ def odomMsgCallBack(odom_msg):
     y_sim = odom_msg.pose.pose.position.y
 
 
-def track():
+def shutCallBack(shut_msg):
+    if shut_msg.data == True:
+        rospy.signal_shutdown("Simulation is finished")
+
+
+def simple_track():
     global x_sim_prev, y_sim_prev, number_of_laps, at_cross_line
+
     i = int(input("Which log file to write to: -1 for automatic detection / any other integer for your choice\n"))
     filename = LOG_PATH + "/logs/trajectory_log_"
-    if i == -1:
+    if i < 0:
         i = 1
         while os.path.exists('{}{:d}.txt'.format(filename, i)):
             i += 1
     filename = '{}{:d}.txt'.format(filename, i)
 
     print("Writing a log file of trajectories to {}".format(filename))
+
+    lap_reload = 5
     try:
         with open(filename, 'a') as tlog:
             while 1:
-                if round(x_sim_prev, 4) != round(x_sim, 4) or round(y_sim_prev, 4) != round(y_sim, 4):
+                # if it's not the first lap and we have moved at least a little in X or Y direction, then log it
+                if number_of_laps > 0 and (round(x_sim_prev, 5) != round(x_sim, 5) or round(y_sim_prev, 5) != round(y_sim, 5)):
                     tlog.write('{:.6f},{:.6f}\n'.format(x_sim, y_sim))
                     x_sim_prev = x_sim
                     y_sim_prev = y_sim
-                    # print('{:5f},{:5f}'.format(round(x_sim, 3), round(y_sim, 3)))
 
+                # if we are at the line where we started, then increase the lap number
+                if abs(round(x_sim, 3) - start_x) < 0.01 and start_y-1 < round(y_sim, 3) < start_y+1:
+                    if not at_cross_line:
+                        at_cross_line = True
+                        number_of_laps = (number_of_laps + 1) % lap_reload
+                        print("New lap. Number {} started".format(number_of_laps))
+                        laps = Int32(data=number_of_laps)
+                        pub3.publish(laps)
+                        r.sleep()
+                else:
+                    at_cross_line = False
+                r.sleep()
+    except Exception as e:
+        print(e)
+
+
+def param_search_track():
+    global x_sim_prev, y_sim_prev, current_lap, at_cross_line
+
+    i = int(input("Which log file to write to: -1 for automatic detection / any other integer for your choice\n"))
+    file_name_ = LOG_PATH + "/logs/trajectory_log_"
+    if i < 0:
+        i = 1
+        while os.path.exists('{}{:d}.txt'.format(file_name_, i)):
+            i += 1
+
+    LAP_RELOAD = 3
+
+    try:
+        while not rospy.is_shutdown():
+            filename = '{}{:d}.txt'.format(file_name_, i)
+            print("Writing a log file of trajectories to {}".format(filename))
+            current_lap = -1
+            with open(filename, 'a') as tlog:
+                while current_lap < LAP_RELOAD:
+                    # if it's not the first lap and we have moved at least a little in X or Y direction, then log it
+                    if current_lap > 0 and (round(x_sim_prev, 5) != round(x_sim, 5) or round(y_sim_prev, 5) != round(y_sim, 5)):
+                        tlog.write('{:.6f},{:.6f}\n'.format(x_sim, y_sim))
+                        x_sim_prev = x_sim
+                        y_sim_prev = y_sim
+
+                    # if we are at the line where we started, then increase the lap number
                     if abs(round(x_sim, 3) - start_x) < 0.01 and start_y-1 < round(y_sim, 3) < start_y+1:
                         if not at_cross_line:
                             at_cross_line = True
-                            number_of_laps += 1
-                            print("New lap. Number {}".format(number_of_laps))
+                            current_lap = current_lap + 1
+                            print("New lap. Number {} started".format(current_lap))
                             r.sleep()
                     else:
                         at_cross_line = False
-                r.sleep()
+
+                    laps = Int32(data=current_lap)
+                    pub3.publish(laps)
+                    r.sleep()
+            i += 1
     except Exception as e:
         print(e)
 
@@ -56,16 +110,19 @@ def track():
 if __name__ == "__main__":
     rospy.init_node('turtlebot3_trajectory')
     odometry_sub = rospy.Subscriber('/odom', Odometry, odomMsgCallBack)
-    pub1 = rospy.Publisher('/gazebo/set_model_state', ModelState, queue_size=5)
-    pub2 = rospy.Publisher('/cmd_vel', Twist, queue_size=5)
-    reset_world = rospy.ServiceProxy("/gazebo/reset_world", Empty)
+    traj_shut_sub = rospy.Subscriber('/paramSearch', Bool, shutCallBack)
+    # pub1 = rospy.Publisher('/gazebo/set_model_state', ModelState, queue_size=5)
+    # pub2 = rospy.Publisher('/cmd_vel', Twist, queue_size=5)
+    pub3 = rospy.Publisher('/laps', Int32, queue_size=5)
+    # reset_world = rospy.ServiceProxy("/gazebo/reset_world", Empty)
 
-    r = rospy.Rate(100)
+    r = rospy.Rate(50)
     r.sleep()
     if (x_sim, y_sim) != (0, 0):
         start_x = round(x_sim, 2)
         start_y = round(y_sim, 2)
         print("Start coordinates are {:.3f},{:.3f}".format(start_x, start_y))
+
     # model_state_msg = ModelState()
     # model_state_msg.model_name = 'turtlebot3_burger'
     # model_state_msg.pose.orientation.x = 0
@@ -77,5 +134,6 @@ if __name__ == "__main__":
     # model_state_msg.pose.position.z = 0
     # model_state_msg.twist.linear.x = 0
     # model_state_msg.twist.angular.z = 0
-    number_of_laps = 1
-    track()
+
+    current_lap = -1
+    param_search_track()
--- scripts/controllers/neural_net.py
+++ scripts/controllers/neural_net.py
@@ -4,6 +4,9 @@ import numpy as np
 import rospy
 from geometry_msgs.msg import Twist
 from sensor_msgs.msg import LaserScan
+from std_msgs.msg import Int32, Bool
+import itertools
+
 
 """"**************************
 * Globals
@@ -14,6 +17,7 @@ half_wheel_separation = 0.08
 front_distance_limit = 0.7
 side_distance_limit = 0.4
 direction_vector = [0, 0, 0, 0, 0]
+angles = [90, 45, 0, -45, -90]
 MAX_TB3_LINEAR = 0.8
 MAX_TB3_ANGULAR = 2.0
 # 5 sensors to the WEST, NW, N, NE, EAST
@@ -24,34 +28,62 @@ NE = 3
 RIGHT = 4
 
 # NN weights 5x2 for every sensor to each wheel
-c = 0.3
-WEIGHTS = [[0, 0, c/3, c, c/2],
-           [c/2, c, c/3, 0, 0]]
-V_bias = [0, 0]
+a = 0.1
+b = 0.1
+c = 0.1
 
+WEIGHTS = [[0, 0, a, b, c],
+            [c, b, a, 0, 0]]
+weight_space = [p for p in itertools.product([0.1, 0.2, 0.3, 0.4, 0.5], repeat=3)]
+V_bias = [0, 0]
+w = -1
+lap_num_zeroed = False
+
+def lapMsgCallBack(weight_msg):
+    global WEIGHTS, a, b, c, w, lap_num_zeroed
+    lap = weight_msg.data
+    if lap == 0:
+        if not lap_num_zeroed:
+            lap_num_zeroed = True
+            if w < 125:
+                w += 1
+                a, b, c = weight_space[w]
+                print(a,b,c)
+                WEIGHTS = [[0, 0, a, b, c],
+                           [c, b, a, 0, 0]]
+                shut_trajectory.publish(Bool(data=False))
+            else:
+                print("Weight space is exhausted. Stop the simulation!")
+                shut_trajectory.publish(Bool(data=True))
+    else:
+        lap_num_zeroed = False
+        shut_trajectory.publish(Bool(data=False))
 
 def laserScanMsgCallBack(laser_msg):
     global direction_vector
     scan = laser_msg.ranges
-    angles = [90, 45, 0, -45, -90]
 
     for counter, angle in enumerate(angles):
         if np.isinf(scan[angle]):
-            direction_vector[counter] = laser_msg.range_max
+            direction_vector[counter] = 1 / laser_msg.range_max
         else:
-            direction_vector[counter] = np.median([scan[angle - 2], scan[angle - 1],
-                                                   scan[angle], scan[angle + 1],
-                                                   scan[angle + 2]])
+            direction_vector[counter] = 1 / np.median([scan[angle - 2], scan[angle - 1],
+                                                       scan[angle], scan[angle + 1],
+                                                       scan[angle + 2]])
 
 
 def set_wheel_velocities(left_wheel_speed=0.0, right_wheel_speed=0.0):
     global V_bias
     cmd_vel = Twist()
+
+    # set linear forward velocity
     cmd_vel.linear.x = (right_wheel_speed + left_wheel_speed) / 2.0
     if cmd_vel.linear.x > MAX_TB3_LINEAR:
-        V_bias = [v-0.05 for v in V_bias]
+        V_bias = [v - 0.05 for v in V_bias]
     elif cmd_vel.linear.x < 0.1:
         V_bias = [v + 0.05 for v in V_bias]
+
+    # set angular velocity
     cmd_vel.angular.z = (right_wheel_speed - left_wheel_speed) / (2 * half_wheel_separation)
     if cmd_vel.angular.z > 0:
         cmd_vel.angular.z = min(MAX_TB3_ANGULAR, cmd_vel.angular.z)
@@ -59,7 +91,7 @@ def set_wheel_velocities(left_wheel_speed=0.0, right_wheel_speed=0.0):
         cmd_vel.angular.z = max(-MAX_TB3_ANGULAR, cmd_vel.angular.z)
 
     cmd_vel_pub.publish(cmd_vel)
-    print(cmd_vel.linear.x, cmd_vel.angular.z, V_bias)
+    # print(cmd_vel.linear.x, cmd_vel.angular.z, V_bias, w)
 
 
 """"*******************************************************************************
@@ -69,8 +101,10 @@ def set_wheel_velocities(left_wheel_speed=0.0, right_wheel_speed=0.0):
 
 def controlLoop():
     # Velocities of L and R wheels are cross-connected to RHS and LHS sensors
-    left_vel = np.dot(WEIGHTS[0], direction_vector) + V_bias[0]
-    right_vel = np.dot(WEIGHTS[1], direction_vector) + V_bias[1]
+    # left_vel = np.dot(WEIGHTS[0], direction_vector) + V_bias[0]
+    # right_vel = np.dot(WEIGHTS[1], direction_vector) + V_bias[1]
+    left_vel = np.dot(WEIGHTS[1], direction_vector) + V_bias[1]
+    right_vel = np.dot(WEIGHTS[0], direction_vector) + V_bias[0]
     set_wheel_velocities(left_wheel_speed=left_vel.item(), right_wheel_speed=right_vel.item())
 
 
@@ -83,7 +117,9 @@ if __name__ == "__main__":
     rospy.loginfo("To stop TurtleBot CTRL + C")
 
     cmd_vel_pub = rospy.Publisher('/cmd_vel', Twist, queue_size=3)
+    shut_trajectory = rospy.Publisher('/paramSearch', Bool, queue_size=3)
     laser_scan_sub = rospy.Subscriber('/scan', LaserScan, laserScanMsgCallBack)
+    laps_sub = rospy.Subscriber('/laps', Int32, lapMsgCallBack)
 
     r = rospy.Rate(125)
     while not rospy.is_shutdown():
